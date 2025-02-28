02c98ddbfb2f26cf4fb096900fdd062266c8dcfd
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-02-27 20:11:57 -0500

    enemy patrol finished working on going close to the player but not to the player

---
+++ Assets/Scripts/EnemyNavigation.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class EnemyNavigation : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Transform[] waypoints;
+    private int wCount;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        wCount = 0; 
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypoints[wCount].position);
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        if (agent.remainingDistance < .1f && wCount < waypoints.Length) 
+        {
+            agent.SetDestination(waypoints[wCount].position);
+            wCount++;
+        }
+        else if (wCount >= waypoints.Length)
+        {
+            wCount = 0;
+            Debug.Log("Executes " + wCount);
+        }
+
+
+    }
+
+    public void closeToPlayer()
+    {
+        Vector3 nearPlayer = new Vector3
+    }
+
+    public void goToPlayer()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
