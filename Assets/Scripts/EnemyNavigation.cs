92de2253755d240f923f7ae7f3ec8d33e2237742
Sze <sz982600@ucf.edu>
2025-02-28 17:12:58 -0500

    updated enemy.cs

--- Assets/Scripts/EnemyNavigation.cs
+++ Assets/Scripts/EnemyNavigation.cs
@@ -15,20 +15,30 @@ public class EnemyNavigation : MonoBehaviour
     private PlayerAudio playerAudio;
     private bool playerNoise;
     private bool isChasing = false;
-    private bool isSearching = false;
+    public bool isSearching = false;
     public GameObject levelCanvas;
+    private Vector3 storePlayerLastPosition;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
-            player.GetComponent<PlayerMovement>().gotCaught = true;
-            transform.LookAt(player.transform.position);
-            Debug.Log("player is caught");
-            levelCanvas.GetComponent<MenuBehavior>().GameOver();
-            agent.isStopped = true;
+            CapturePlayer();
         }
     }
+
+    private void CapturePlayer()
+    {
+        player.GetComponent<PlayerMovement>().gotCaught = true;
+        transform.LookAt(player.transform.position);
+        Debug.Log("player is caught");
+        levelCanvas.GetComponent<MenuBehavior>().GameOver();
+        agent.isStopped = true;
+        player.GetComponent<MouseLook>().enabled = false;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -55,7 +65,9 @@ public class EnemyNavigation : MonoBehaviour
         else if (isChasing)
         {
             isChasing = false;
-            StartCoroutine(SearchArea());
+            StartCoroutine(GotoLastHeard());
+
+
         }
         else
         {
@@ -95,6 +107,8 @@ public class EnemyNavigation : MonoBehaviour
 
             Vector3 closetoplayer = new Vector3(player.transform.position.x - 1f, player.transform.position.y, player.transform.position.z - 1f);
             agent.SetDestination(closetoplayer);
+            storePlayerLastPosition = player.transform.position;
+
         }
     }
 
@@ -103,26 +117,51 @@ public class EnemyNavigation : MonoBehaviour
         agent.SetDestination(player.transform.position);
     }
 
-    IEnumerator SearchArea()
+
+
+    IEnumerator GotoLastHeard()
     {
         isSearching = true;
+        agent.SetDestination(storePlayerLastPosition);
+        Debug.Log("Moving to last heard position...");
 
-        for (int i = 0; i < 2; i++)
+        // **Ensure the enemy reaches the last heard position before searching**
+        while (agent.remainingDistance < 0.5f)
         {
-            Vector3 randomSearchPos = transform.position + new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f)
-            );
+            yield return null;
+        }
+        Debug.Log("Reached last heard position. Searching...");
+        yield return StartCoroutine(SearchArea());
+    }
 
+    IEnumerator SearchArea()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 randomSearchPos = storePlayerLastPosition + new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f));
             agent.SetDestination(randomSearchPos);
+<<<<<<< Updated upstream
             Debug.Log("Give up but keep searching: " + i);
+=======
+            Debug.Log($"Searching area {i + 1}");
+>>>>>>> Stashed changes
 
-            yield return new WaitForSeconds(4);
+
+            yield return new WaitForSeconds(2f);
         }
 
+<<<<<<< Updated upstream
         Debug.Log("Give up");
 
+=======
+        Debug.Log("Search finished, returning to patrol.");
+>>>>>>> Stashed changes
         isSearching = false;
         ReturnToPatrol();
     }
+
+
+
 
     void ReturnToPatrol()
     {
