e97281ae741122b6f133fb7793e3ca6b7613769e
popoyo07 <piwedaa@gmail.com>
2025-02-28 14:38:15 -0500

    resolution for the game should be 1920 by 1080
     Screen.SetResolution(1920, 1080, true);

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -16,7 +16,10 @@ public class MenuBehavior : MonoBehaviour
     {
         SceneManager.LoadScene("Level");
     }
-
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
     // Update is called once per frame
     void Update()
     {
